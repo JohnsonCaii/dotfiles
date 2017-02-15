021dacd1ccfecca02996148aa206dc021bc61bc5
johnson <johnson.cai@btcc.com>
2017-02-15 22:33:04 +0800

    add emacs conf

---
+++ 1/elpa/todotxt-20150513.1929/todotxt-autoloads.el
@@ -0,0 +1,15 @@
+;;; todotxt-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("todotxt.el") (21861 19936 381270 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; todotxt-autoloads.el ends here
---
+++ 1/plugins/workgroups2-conf.el
@@ -0,0 +1,26 @@
+;;; workgroups2
+(require 'workgroups2)
+
+(setq wg-prefix-key (kbd "C-c w")
+      wg-restore-associated-buffers nil ; restore all buffers opened in this WG?
+      wg-use-default-session-file nil   ; turn off for "emacs --daemon"
+      wg-default-session-file "~/.emacs.d/savefile/.workspace2"
+      wg-use-faces nil
+      wg-morph-on nil)         ; animation off
+;; Keyboard shortcuts - load, save, switch
+
+(global-set-key (kbd "s-z") 'wg-switch-to-workgroup)
+(global-set-key (kbd "C-c w f") 'wg-switch-to-workgroup)
+(global-set-key (kbd "C-c w w") 'wg-switch-to-previous-workgroup)
+
+(condition-case ex
+    (workgroups-mode 1)          ; Activate workgroups
+  ('error (message "wrong in workgroup init")))
+
+(desktop-save-mode 0)
+
+(global-set-key (kbd "C-c w .") 'wg-reload-session)
+(if (and (fboundp 'daemonp) (daemonp))
+    ()
+  ;; (add-hook 'after-make-frame-functions  'load-wg-session)
+  (wg-reload-session))
