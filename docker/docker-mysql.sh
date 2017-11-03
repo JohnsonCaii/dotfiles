ad97381bcf38229a4cf46b65037a91e2ef568c33
Johnson Cai <johnson.cai@btcc.com>
2017-11-03 18:18:38 +0800

    add scripts

--- docker/docker-mysql.sh
+++ docker/docker-mysql.sh
@@ -1,1 +1,2 @@
-/Users/zhpooer/docker/mysql/docker-mysql.sh
+#!/bin/bash
+docker run -d -p 3306:3306 -v ~/docker/mysql/data:/var/lib/mysql -v ~/docker/mysql/conf:/etc/mysql/conf.d --name mysql -e MYSQL_ROOT_PASSWORD=root mysql
--- docker/docker-redis.sh
+++ docker/docker-redis.sh
@@ -1,1 +1,2 @@
-/Users/zhpooer/docker/redis/docker-redis.sh
+#!/bin/bash
+docker run -d -p 6379:6379 -v ~/docker/redis/data:/data --name redis redis
