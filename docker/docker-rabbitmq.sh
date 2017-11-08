b9f263d3ce3139c6bdf7cfa4c703d40106c75129
Johnson Cai <johnson.cai@btcc.com>
2017-11-08 17:35:01 +0800

    add rabbitmq

---
+++ docker/docker-rabbitmq.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker run -d --name rabbitmq \
+    --publish 5671:5671 \
+    --publish 5672:5672 \
+    --publish 4369:4369 \
+    --publish 25672:25672 \
+    --publish 15671:15671 \
+    --publish 15672:15672 \
+    --hostname rabbit-server \
+    -v ~/docker/rabbitmq/data:/var/lib/rabbitmq \
+    -v ~/docker/rabbitmq/conf:/etc/rabbitmq \
+    rabbitmq:management
