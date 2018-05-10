c63cf5c7193fb9b1f1079cb33bf9ea3e6904113a
johnson.cai@btcc.com <johnson.cai@btcc.com>
2018-05-11 07:44:43 +0800

    add arch config file

--- .emacs.d/elpa/todotxt-20150513.1929/todotxt-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "todotxt" "20150513.1929" "A major mode for editing todo.txt files" 'nil :url "https://github.com/rpdillon/todotxt.el" :keywords '("todo.txt" "todotxt" "todotxt.el"))
