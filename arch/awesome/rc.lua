d9712ff27bc58ddb1205fde0294be7bef1c71e94
johnson.cai@btcc.com <johnson.cai@btcc.com>
2018-06-24 23:46:12 +0800

    update config

--- arch/awesome/rc.lua
+++ arch/awesome/rc.lua
@@ -43,6 +43,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.font = "DejaVu Sans 10"
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
@@ -54,7 +55,7 @@ editor_cmd = terminal .. " -e " .. editor
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod1"
+modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
