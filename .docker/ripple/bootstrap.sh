97a1e774c6c3474edd4da03d80b76755dd28d6d4
johnson.cai@btcc.com <johnson.cai@btcc.com>
2018-07-29 08:15:35 +0800

    add i3 vm config and remove awesome config

--- .docker/ripple/bootstrap.sh
+++ .docker/ripple/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -p 5005:5005 \
+docker run -m 512m --cpus='1' -d -p 5005:5005 \
        -v ~/dotfiles/.docker/ripple/rippled.cfg:/etc/ripple/rippled.cfg \
        -v ~/dotfiles/.docker/ripple/validators.txt:/etc/ripple/validators.txt \
        --name rippled rippled
--- mac-conf/docker-scripts.sh
+++ mac-conf/docker-scripts.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 run_mysql() {
-    docker run -d -p 3306:3306 \
+    docker run -m 128m --cpus='1' -d -p 3306:3306 \
            -v ~/.docker/mysql/data:/var/lib/mysql \
            -v ~/.docker/mysql/conf:/etc/mysql/conf.d \
            --restart=always --name mysql -e MYSQL_ROOT_PASSWORD=root mysql
 }
 
 run_rabbitmq() {
-    docker run -d --name rabbitmq \
+    docker run -m 128m --cpus='1' -d --name rabbitmq \
            --publish 5671:5671 \
            --publish 5672:5672 \
            --publish 4369:4369 \
@@ -19,18 +19,18 @@ run_rabbitmq() {
            --restart=always \
            -v ~/.docker/rabbitmq/data:/var/lib/rabbitmq \
            -v ~/.docker/rabbitmq/conf:/etc/rabbitmq \
-           rabbitmq:management
+           rabbitmq:3.6.1-management
     docker exec rabbitmq rabbitmq-plugins enable rabbitmq_management
 }
 
 run_redis() {
-    docker run -d -p 6379:6379 \
+    docker run -m 128m --cpus='1' -d -p 6379:6379 \
            -v ~/.docker/redis/data:/data \
            --restart=always --name redis redis
 }
 
 run_postgres() {
-    docker run --name postgres -p 5432:5432 \
+    docker run -m 128m --cpus='1' --name postgres -p 5432:5432 \
            -e POSTGRES_PASSWORD=root --restart=always -d postgres
 }
 
