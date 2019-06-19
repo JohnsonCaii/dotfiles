607691ec712b51ce25b44fe9179ae01e1c0f5af4
Johnson Cai <caiezq@gmail.com>
2019-06-19 10:59:04 +0800

    remove docker

--- .docker/ripple/prod/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -m 4096m --cpus='2' -d -p 5006:5006 \
-       -v ~/dotfiles/.docker/ripple/prod/rippled.cfg:/etc/ripple/rippled.cfg \
-       -v ~/dotfiles/.docker/ripple/prod/validators.txt:/etc/ripple/validators.txt \
-       --name prod-rippled rippled
-
